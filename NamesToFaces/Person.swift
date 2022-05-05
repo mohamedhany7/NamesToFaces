b938fcaaa5796b741a1d64ded6c83aa1ffb5e5df
Mohamed Hany <mohameddhanyy7@gmail.com>
2022-05-06 00:54:34 +0200

    Second commit

---
+++ NamesToFaces/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  NamesToFaces
+//
+//  Created by Mohamed Hany on 05/05/2022.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
---
+++ NamesToFaces/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by Mohamed Hany on 25/04/2022.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
